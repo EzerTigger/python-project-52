8b7701fe645ea11f48c3bd672ba96dfff1c43d04
Sergei Ezerskii <gaftezer@gmail.com>
2023-12-13 15:05:18 +0300

    Add link to README

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['webserver', '127.0.0.1']
 
