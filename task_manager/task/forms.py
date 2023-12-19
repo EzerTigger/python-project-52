5cc76c36c8e9b995df703b19cc528e880776c7cc
Sergei Ezerskii <gaftezer@gmail.com>
2023-12-19 16:14:11 +0300

    Realize confirm password in create form

--- task_manager/task/forms.py
+++ task_manager/task/forms.py
@@ -4,22 +4,28 @@ from .models import *
 
 
 class CreateUserForm(forms.ModelForm):
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = [
             'first_name',
             'last_name',
             'username',
-            'password'
+            'password',
+            'confirm_password'
         ]
+
+        labels = {}
+
         widgets = {
-            'password': forms.PasswordInput()
+            'password': forms.PasswordInput(),
         }
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password'])
-        if commit:
+        if commit and self.cleaned_data['password'] == \
+                self.cleaned_data['confirm_password']:
             user.save()
         return user
-
