bb0c121a1aedfd10950464b5fd766dce6e5e46ec
Sergei Ezerskii <gaftezer@gmail.com>
2024-01-09 13:52:07 +0300

    Start making create user tests

--- task_manager/task/tests/test_main_page.py
+++ task_manager/task/tests/test_main_page.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 
 
-class TaskTestCase(TestCase):
+class MainPageTest(TestCase):
 
-    def test_index(self):
+    def test_main(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
---
+++ task_manager/task/tests/users_tests/test_create.py
@@ -0,0 +1,28 @@
+import json
+import os
+from django.test import TestCase
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+
+
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    '../fixtures'
+)
+
+
+class CreateUserTest(TestCase):
+
+    def test_open_create_page(self):
+        response = self.client.get(reverse_lazy('create_user'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user(self):
+        fixture = os.path.join(FIXTURE_DIR, 'user.json')
+        test_user = json.load(open(fixture))
+        response = self.client.post(
+            reverse_lazy('create_user'),
+            test_user
+        )
+        user = User.objects.get(pk=1)
+        self.assertEqual(user.username, test_user.get('username'))
