cf78cbdf67f2f810baa176412f2b8088a0437e3e
Sergei Ezerskii <gaftezer@gmail.com>
2024-01-10 11:35:47 +0300

    Fix test_delete

--- task_manager/task/tests/users_tests/test_delete.py
+++ task_manager/task/tests/users_tests/test_delete.py
@@ -1,14 +1,26 @@
+import os
+import json
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 
 
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    '../fixtures'
+)
+
+
 class DeleteUserTest(TestCase):
 
     def test_delete(self):
-        user = User.objects.create_user(username='Vasya', password='qwerty')
+        fixture = os.path.join(FIXTURE_DIR, 'user.json')
+        test_user = json.load(open(fixture))
+        username = test_user['username']
+        password = test_user['password']
+        user = User.objects.create_user(username=username, password=password)
         self.assertIn(user, User.objects.all())
-        self.client.login(username='Vasya', password='qwerty')
+        self.client.login(username=username, password=password)
         self.client.post(
             reverse_lazy(
                 'delete',
