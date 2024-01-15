d2d72a14ed3a859d02893978bbafd7117ed7d5e7
Sergei Ezerskii <gaftezer@gmail.com>
2024-01-15 20:46:20 +0300

    Add message for create status

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create/', views.CreateStatusView.as_view(), name='create_status'),
-    path('', views.StatusesList.as_view(), name='statuses_list'),
+    path('', views.StatusesList.as_view(), name='status_list'),
 ]
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,15 +1,21 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
-
+from django.contrib import messages
 from task_manager.statuses.forms import StatusForm
 from task_manager.statuses.models import Status
+from django.utils.translation import gettext_lazy as _
 
 
-class CreateStatusView(CreateView):
+class CreateStatusView(CreateView, SuccessMessageMixin):
     form_class = StatusForm
     template_name = 'statuses/create_status.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('status_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, _('Status successfully created'))
+        return super().form_valid(form)
 
 
 class StatusesList(ListView):
