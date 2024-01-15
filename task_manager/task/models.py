1f0724403ef6ac00c84021c49d544b6170cdd6de
Sergei Ezerskii <gaftezer@gmail.com>
2024-01-15 12:27:30 +0300

    Add and register Status model

--- task_manager/task/models.py
+++ task_manager/task/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 
+class Status(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
