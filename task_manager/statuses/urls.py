66e67e9a5f27c0039200932ac95a47233d684074
Sergei Ezerskii <gaftezer@gmail.com>
2024-01-16 15:09:04 +0300

    Realize update status

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('create/', views.CreateStatusView.as_view(), name='create_status'),
-    path('<int:pk>/delete/', views.DeleteStatusView.as_view(), name='delete_status'),
+    path('<int:pk>/delete/', views.DeleteStatusView.as_view(),
+         name='delete_status'),
     path('', views.StatusesList.as_view(), name='status_list'),
+    path('<int:pk>/update/', views.UpdateStatusView.as_view(),
+         name='update_status'),
 ]
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,7 +1,7 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DeleteView
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from django.contrib import messages
 from task_manager.statuses.forms import StatusForm
 from task_manager.statuses.models import Status
@@ -24,6 +24,16 @@ class CreateStatusView(LoginRequiredCustomMixin, CreateView, SuccessMessageMixin
 class StatusesList(LoginRequiredCustomMixin, ListView):
     model = Status
     permission_denied_message = _('You are not logged in. Please log in')
+
+
+class UpdateStatusView(LoginRequiredCustomMixin, SuccessMessageMixin, UpdateView):
+
+    model = Status
+    form_class = StatusForm
+    template_name = "statuses/status_update.html"
+    success_url = reverse_lazy('status_list')
+    permission_denied_message = _('You are not logged in. Please log in')
+    success_message = _('Status update successfully')
 
 
 class DeleteStatusView(LoginRequiredCustomMixin, DeleteView):
