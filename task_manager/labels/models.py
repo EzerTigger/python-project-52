f97b2ea375a93610ea75cac746a813ce306f9bbb
Sergei Ezerskii <gaftezer@gmail.com>
2024-01-23 16:26:55 +0300

    Create Label model

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from task_manager.tasks.models import Task
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    tasks = models.ManyToManyField(Task)
