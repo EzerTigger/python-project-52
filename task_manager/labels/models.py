1622706afff75c6da24a8ce54ed3b9b25ecf7818
Sergei Ezerskii <gaftezer@gmail.com>
2024-01-23 17:00:18 +0300

    Add meta in Label model

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -7,3 +7,10 @@ class Label(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateField(auto_now_add=True)
     tasks = models.ManyToManyField(Task)
+
+    class Meta:
+        verbose_name_plural = 'Метки'
+        verbose_name = 'Метка'
+
+    def __str__(self):
+        return self.name
