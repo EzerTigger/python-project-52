6131a983eebfc1fdf5a624ed03d3f1156ae96a88
Sergei Ezerskii <gaftezer@gmail.com>
2024-01-23 12:16:09 +0300

    Fix field's necessity in Task model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -7,10 +7,10 @@ from task_manager.statuses.models import Status
 
 class Task(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
     executor = models.ForeignKey(User, on_delete=models.PROTECT,
-                                 related_name='tasks')
+                                 related_name='tasks', blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT,
                                related_name='created_tasks')
     created_at = models.DateField(auto_now_add=True)
