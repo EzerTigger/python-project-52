594dfd4915c3281791b9f008e22c08c05d3a875a
Sergei Ezerskii <gaftezer@gmail.com>
2024-01-24 15:27:48 +0300

    Realize create label

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('create/', views.CreateStatusView.as_view(), name='create_status'),
+    path('create/', views.CreateLabelView.as_view(), name='create_label'),
     # path('<int:pk>/delete/', views.DeleteStatusView.as_view(),
         # name='delete_status'),
     path('', views.LabelsList.as_view(), name='label_list'),
--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from django.contrib import messages
+
+from task_manager.labels.forms import LabelForm
 from task_manager.labels.models import Label
 
 from django.utils.translation import gettext_lazy as _
@@ -13,3 +15,11 @@ from task_manager.users.mixins import LoginRequiredCustomMixin
 class LabelsList(LoginRequiredCustomMixin, ListView):
     model = Label
     permission_denied_message = _('You are not logged in. Please log in')
+
+
+class CreateLabelView(LoginRequiredCustomMixin, SuccessMessageMixin, CreateView):
+    form_class = LabelForm
+    template_name = 'labels/create_label.html'
+    success_url = reverse_lazy('label_list')
+    permission_denied_message = _('You are not logged in. Please log in')
+    success_message = _('Label successfully created')
