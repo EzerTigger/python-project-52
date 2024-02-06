55355e134f9aa5fb5a606bb1252f6ea2552c01b6
Sergei Ezerskii <gaftezer@gmail.com>
2024-02-06 12:06:27 +0300

    Finish i18n

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,10 +1,23 @@
 import django_filters
 
+from task_manager.labels.models import Label
+from task_manager.statuses.models import Status
 from task_manager.tasks.models import Task
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class TaskFilter(django_filters.FilterSet):
-
+    status = django_filters.ModelChoiceFilter(label=_('Status'),
+                                              queryset=Status.objects.all())
+    executor = django_filters.ModelChoiceFilter(label=_('Executor'),
+                                                queryset=User.objects.all())
+    labels = django_filters.ModelMultipleChoiceFilter(
+        label=_('Labels'),
+        queryset=Label.objects.all()
+    )
+ 
     class Meta:
         model = Task
         fields = ['status', 'executor', 'labels']
+
