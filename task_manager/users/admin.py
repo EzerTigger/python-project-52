c67be205b6dc20adae76201ddeba87be6492b303
Sergei Ezerskii <gaftezer@gmail.com>
2024-02-09 10:28:39 +0300

    Override __str__ method of User model

--- task_manager/users/admin.py
+++ task_manager/users/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+
+def get_full_name(self):
+    return self.first_name + ' ' + self.last_name
+
+
+User.__str__ = get_full_name
 
 
 
