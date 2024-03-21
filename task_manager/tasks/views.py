b7d644f69e3df36e6bc6c06bf3a092c25c2c7585
Sergei Ezerskii <gaftezer@gmail.com>
2024-03-21 17:32:54 +0300

    Fix linter

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,6 +1,6 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DeleteView, UpdateView, DetailView
+from django.views.generic import CreateView, DeleteView, UpdateView, DetailView
 from django.utils.translation import gettext_lazy as _
 from django_filters.views import FilterView
 
